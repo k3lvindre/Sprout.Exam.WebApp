9749b93ea1bc28dfa482b1b32d323252bfe39704
k3lvindre <k3lvindre@gmail.com>
2024-01-14 21:40:09 +0800

    Added core project

---
+++ Sprout.Exam.Core/EmployeeAggregate/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Sprout.Exam.Core.EmployeeAggregate
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Birthdate { get; set; }
+        public string Tin { get; set; }
+        public int EmployeeTypeId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Sprout.Exam.Core/EmployeeAggregate/Enums/EmployeeType.cs
@@ -0,0 +1,8 @@
+﻿namespace Sprout.Exam.Core.EmployeeAggregate.Enums
+{
+    public enum EmployeeType
+    {
+        Regular = 1,
+        Contractual = 2
+    }
+}
