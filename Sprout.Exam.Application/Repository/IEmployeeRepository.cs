fc10dcb19bdc777587b18a6b7dc98347d3520078
k3lvindre <k3lvindre@gmail.com>
2024-01-14 21:40:27 +0800

    added application project

---
+++ Sprout.Exam.Application/Repository/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Sprout.Exam.Core.EmployeeAggregate;
+using System.Threading.Tasks;
+
+namespace Sprout.Exam.Application
+{
+    public interface IEmployeeRepository
+    {
+        Task<int> CreateEmployee(Employee employee);
+    }
+}
