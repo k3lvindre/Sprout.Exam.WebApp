1749d58e61d41448188cf5966eb98cc634b6b863
k3lvindre <k3lvindre@gmail.com>
2024-01-14 21:41:24 +0800

    Added infrastructure project and sproutexamcontext with employee repo implementation

---
+++ Sprout.Exam.Infrastructure/EntityFramework/Repository/EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Sprout.Exam.Application;
+using Sprout.Exam.Core.EmployeeAggregate;
+using System.Threading.Tasks;
+
+namespace Sprout.Exam.Infrastructure.EntityFramework.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly SproutExamContext _sproutExamContext;
+
+        public EmployeeRepository(SproutExamContext sproutExamContext)
+        {
+            _sproutExamContext = sproutExamContext;
+        }
+
+        public async Task<int> CreateEmployee(Employee employee)
+        {
+            await _sproutExamContext.Employees.AddAsync(employee);
+            return await _sproutExamContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Sprout.Exam.Infrastructure/EntityFramework/SproutExamContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sprout.Exam.Core.EmployeeAggregate;
+
+namespace Sprout.Exam.Infrastructure
+{
+    public class SproutExamContext : DbContext
+    {
+        public SproutExamContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("dbo");
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+    }
+}
