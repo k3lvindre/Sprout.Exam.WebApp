e891b18a06fe0f880aac0af6386f3de3e1a9fa77
k3lvindre <k3lvindre@gmail.com>
2024-01-15 09:54:32 +0800

    added CalculateSalary implementation to regularemployee.

--- Sprout.Exam.Core/EmployeeAggregate/RegularEmployee.cs
+++ Sprout.Exam.Core/EmployeeAggregate/RegularEmployee.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Options;
 
 namespace Sprout.Exam.Core.EmployeeAggregate
 {
     public class RegularEmployee : Employee
     {
-        public override void CalculateSalary()
-        {
-            throw new NotImplementedException();
+        public RegularEmployee() 
+        {}
+
+        public override decimal CalculateSalary(decimal days, SalaryOption salaryOption) {
+            var regularEmployeeSalaryOption = salaryOption as RegularEmployeeSalaryOption;
+            if (regularEmployeeSalaryOption is null) return 0;
+
+            var salary = regularEmployeeSalaryOption.Salary - GetSalaryDeduction() - GetTaxDeduction();
+            return Math.Round(salary, 2);
+
+            //days = number of days absent
+            decimal GetSalaryDeduction() => (regularEmployeeSalaryOption.Salary / regularEmployeeSalaryOption.Dividend) * days;
+            decimal GetTaxDeduction() => regularEmployeeSalaryOption.Salary * regularEmployeeSalaryOption.Tax;
         }
     }
 }
